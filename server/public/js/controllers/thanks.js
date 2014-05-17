1f97b0f953165c60538736463c83af112e871c32
loumontano <montano.lourdes@gmail.com>
2014-05-17 11:56:16 -0300

    thanks page

--- server/public/js/controllers/thanks.js
+++ server/public/js/controllers/thanks.js
@@ -3,16 +3,16 @@ angular.module('tachoApp.controllers')
 
     $scope.counter = 15;
     $scope.status = pointsFactory.currentState;
-    // $scope.onTimeout = function(){
+    $scope.onTimeout = function(){
 
-    //   if ($scope.counter-1 < 0){
-    //     //Save points and freeze screen...
-    //     $location.path('/idle');
-    //   }
-    //   else {
-    //     $scope.counter--;
-    //     mytimeout = $timeout($scope.onTimeout,1000);
-    //   }
-    // }
-    //var mytimeout = $timeout($scope.onTimeout,1000);
+      if ($scope.counter-1 < 0){
+        //Save points and freeze screen...
+        $location.path('/idle');
+      }
+      else {
+        $scope.counter--;
+        mytimeout = $timeout($scope.onTimeout,1000);
+      }
+    }
+    var mytimeout = $timeout($scope.onTimeout,1000);
 });
